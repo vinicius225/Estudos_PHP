a4b201ecc5418974f04cbe068cdbeb52f28dd76d
vinicius225 <vinicius.guilherme225@gmail.com>
2021-09-15 21:57:39 -0300

    first commit

---
+++ Variaveis.php
@@ -0,0 +1,16 @@
+
+<?php
+
+$nome = "Vinicius";
+$idade = 23;
+$altura = 1.73 ;
+
+echo  " Meu nome é $nome, minha  idade é $idadae, altura é $altura" ;
+
+$nome = "Alice";
+$idade = 30;
+$altura = 1.50 ;
+
+echo  " Meu nome é $nome, minha  idade é $idadae, altura é $altura" ;
+
+?>
